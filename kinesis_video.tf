88174b51fa87881467e66a7cb0428ea74326f68c
Robert Hockley <robert.hockley@accenture.com>
2025-04-21 11:17:44 +0800

    kvs

--- kinesis_video.tf
+++ kinesis_video.tf
@@ -1,5 +1,5 @@
 resource "aws_kinesis_video_stream" "default" {
-  name                    = "streams-${var.connect.alias}-connect-contact"
+  name                    = "streams-connect-${var.connect.alias}-contact"
   data_retention_in_hours = 1
   device_name             = "kinesis-video-device-name"
   media_type              = "video/h264"
