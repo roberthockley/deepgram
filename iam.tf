5fb4daf0e677a95b5858872cef8c2737411d62ea
Robert Hockley <robert.hockley@accenture.com>
2025-04-21 18:27:18 +0800

    Update

--- iam.tf
+++ iam.tf
@@ -1,3 +1,70 @@
+resource "aws_iam_role" "kvs_dg_integrator_execution_role" {
+  name = "kvsDgIntegratorExecutionRole2"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "kvs_dg_integrator_cloudwatch" {
+  name        = "PolicyForCloudwatch"
+  path        = "/"
+  description = "Policy to allow Lambda to write to Cloudwatch on each invokation"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "logs:CreateLogGroup"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_ecs" {
+  depends_on = []
+  role       = aws_iam_role.kvs_dg_integrator_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_cloudwatch" {
+  depends_on = [aws_iam_policy.kvs_dg_integrator_cloudwatch]
+  role       = aws_iam_role.kvs_dg_integrator_execution_role.name
+  policy_arn = aws_iam_policy.kvs_dg_integrator_cloudwatch.arn
+}
+
+resource "aws_iam_role" "kvs_dg_integrator_task_role" {
+  name = "kvsDgIntegratorTaskRole2"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_task_kvs" {
+  depends_on = []
+  role       = aws_iam_role.kvs_dg_integrator_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisVideoStreamsReadOnlyAccess"
+}
+
+
 resource "aws_iam_role" "dg_trigger_role" {
   name = "Deepgram-kvsDgTriggerRole"
   assume_role_policy = jsonencode({
