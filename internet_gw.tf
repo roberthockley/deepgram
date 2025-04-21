5e9caa9133cf82acf05a13c3a99ad382ae7b6969
Robert Hockley <robert.hockley@accenture.com>
2025-04-21 12:35:16 +0800

    IGW

--- internet_gw.tf
+++ internet_gw.tf
@@ -4,8 +4,3 @@ resource "aws_internet_gateway" "deepgram" {
     Name = "Deepgram"
   }
 }
-
-resource "aws_internet_gateway_attachment" "deepgram" {
-  internet_gateway_id = aws_internet_gateway.deepgram.id
-  vpc_id              = aws_vpc.deepgram.id
-}
