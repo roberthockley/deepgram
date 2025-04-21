e2f98d8415267dc53fa993e48bda298b75a56409
Robert Hockley <robert.hockley@accenture.com>
2025-04-21 12:54:34 +0800

    ecs

--- iam.tf
+++ iam.tf
@@ -1,14 +1,14 @@
 resource "aws_iam_role" "kvs_dg_integrator_execution_role" {
-  name               = "kvsDgIntegratorExecutionRole"
+  name = "kvsDgIntegratorExecutionRole"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect    = "Allow"
+        Effect = "Allow"
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
-        Action    = "sts:AssumeRole"
+        Action = "sts:AssumeRole"
       }
     ]
   })
@@ -21,23 +21,23 @@ resource "aws_iam_policy" "kvs_dg_integrator_cloudwatch" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-        {
-          Effect   = "Allow"
-          Action   = "logs:CreateLogGroup"
-          Resource = "*"
-        }
-      ]
+      {
+        Effect   = "Allow"
+        Action   = "logs:CreateLogGroup"
+        Resource = "*"
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_execution_role" {
-    depends_on = [  ]
+resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_ecs" {
+  depends_on = []
   role       = aws_iam_role.kvs_dg_integrator_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_execution_role" {
-    depends_on = [ aws_iam_policy.kvs_dg_integrator_cloudwatch ]
+resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_cloudwatch" {
+  depends_on = [aws_iam_policy.kvs_dg_integrator_cloudwatch]
   role       = aws_iam_role.kvs_dg_integrator_execution_role.name
   policy_arn = aws_iam_policy.kvs_dg_integrator_cloudwatch.arn
 }
