baf7b65becc7a2cf630843bc45774adc6a48e630
Robert Hockley <robert.hockley@accenture.com>
2025-04-21 16:34:08 +0800

    security group

--- iam.tf
+++ iam.tf
@@ -64,3 +64,26 @@ resource "aws_iam_role_policy_attachment" "kvs_dg_integrator_task_kvs" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisVideoStreamsReadOnlyAccess"
 }
 
+
+resource "aws_iam_role" "dg_trigger_role" {
+  name = "Deepgram-kvsDgTriggerRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "dg_trigger_role" {
+  depends_on = []
+  role       = aws_iam_role.dg_trigger_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
--- security_group.tf
+++ security_group.tf
@@ -1,8 +1,7 @@
-resource "aws_security_group" "kvs_dg_trigger_sg" {
-  name        = "kvs-dg-trigger-sg"
-  description = "Security group for kvs-dg-trigger Lambda function"
+resource "aws_security_group" "kvsDgIntegratorEcs" {
+  name        = "Deepgram-kvsDgIntegratorEcsServiceSecurityGroup"
   vpc_id      = aws_vpc.deepgram.id
-
+  description = "Security group for kvs-dg-integrator Fargate task"
   egress {
     from_port   = 0
     to_port     = 0
@@ -10,4 +9,60 @@ resource "aws_security_group" "kvs_dg_trigger_sg" {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Allow all outbound traffic"
   }
+  tags = {
+    Name = "Deepgram-kvsDgIntegratorEcsServiceSecurityGroup"
+  }
+}
+
+resource "aws_security_group_rule" "kvsDgIntegratorEcs" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.kvsDgIntegratorLoadBalancer.id
+  security_group_id        = aws_security_group.kvsDgIntegratorEcs.id
+  description              = "Allow HTTP from source security group"
+}
+
+
+resource "aws_security_group" "kvsDgTrigger" {
+  name        = "Deepgram-kvsDgTriggerSecurityGroup"
+  vpc_id      = aws_vpc.deepgram.id
+  description = "Security group for kvs-dg-trigger Lambda function"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+  tags = {
+    Name = "Deepgram-kvsDgTriggerSecurityGroup"
+  }
+}
+
+resource "aws_security_group" "kvsDgIntegratorLoadBalancer" {
+  name        = "Deepgram-kvsDgIntegratorLoadBalancerSecurityGroup"
+  vpc_id      = aws_vpc.deepgram.id
+  description = "Security group for kvs-dg-integrator load balancer"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+  tags = {
+    Name = "Deepgram-kvsDgIntegratorLoadBalancerSecurityGroup"
+  }
+}
+
+resource "aws_security_group_rule" "kvsDgIntegratorLoadBalancer" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.kvsDgIntegratorLoadBalancer.id
+  security_group_id        = aws_security_group.kvsDgIntegratorLoadBalancer.id
+  description              = "Allow HTTP from source security group"
 }
